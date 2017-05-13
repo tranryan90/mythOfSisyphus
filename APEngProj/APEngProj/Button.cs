e517eac1ea9f178f860fb019ca8ea82151f01a0b
tranryan90 <Tranryan90@gmail.com>
2017-05-12 22:25:55 -0400

    Add files via upload
    This is our basic stuff

---
+++ APEngProj/APEngProj/Button.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+
+namespace APEngProj
+{
+    public class Button
+    {
+        public Texture2D texture;
+        public Vector2 position;
+        public Rectangle rectangle;
+        private ButtonState oldState;
+        private ButtonState newState;
+
+        Color color = new Color(255, 255, 255, 255);
+        public Vector2 size;
+
+        public Button(Texture2D newTexture ,GraphicsDevice graphicsMger, EventHandler onClick = null)
+        {
+            texture = newTexture;
+            size = new Vector2(graphicsMger.Viewport.Width / 8, graphicsMger.Viewport.Height/30);
+            ButtonClicked = onClick;
+            oldState = ButtonState.Released;
+            newState = ButtonState.Released;
+        }
+
+        bool down;
+        public void Update()
+        {
+            rectangle = new Rectangle((int)position.X,(int)position.Y,)
+
+        }
+        public event EventHandler ButtonClicked;
+        public void OnButtonClicked()
+        {
+            ButtonClicked?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ APEngProj/APEngProj/ScreenManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Audio;
+
+namespace APEngProj
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class ScreenManager : Game
+    {
+        public static GraphicsDeviceManager graphicsMger;
+        public static SpriteBatch spriteBatch;
+        public static SpriteBatch Sprites;
+        public static Dictionary<string, Texture2D> Textures2D;
+        public static Dictionary<string, Screen> Screens;
+
+        public ScreenManager()
+        {
+            graphicsMger = new GraphicsDeviceManager(this);
+            graphicsMger.PreferredBackBufferHeight = 800;
+            graphicsMger.PreferredBackBufferWidth = 800;
+            Content.RootDirectory = "Content";
+            Screens = new Dictionary<string, Screen>();
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            Textures2D = new Dictionary<string, Texture2D>();
+            this.IsMouseVisible = true;
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            //Where we are gonna add event handling
+
+            Content = base.Content;
+            //Adding screens to the dictionary
+
+            base.LoadContent();
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            //going to add so if you click cer
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+        public void HandleButtonClicked(object sender, EventArgs eventArgs)
+        {
+            //Need to make some other parts before we can start this 
+            //probably need to make title and game screen
+        }
+    }
+}
